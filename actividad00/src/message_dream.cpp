a5059afcf580012b3c4b45e9f623cca18a795146
jgsogo <jgsogo@gmail.com>
2021-02-02 20:32:19 +0100

    not yet

--- actividad00/src/message_dream.cpp
+++ actividad00/src/message_dream.cpp
@@ -4,17 +4,5 @@
 constexpr int factor_temporal = 3;
 
 std::string message_dream(std::string word, int level) {
-    if (level == 0) { return word; }
-    else {
-        // Every level the letters are repeated N times
-        std::string ret;
-        for (auto c: word) {
-            int times = (c == ' ') ? 1 : factor_temporal;
-            for (auto i=0; i<times; i++) {
-                ret.push_back(c);
-            }
-        }
-        // And pass the message to the next level
-        return message_dream(ret, level - 1);
-    }
+    return "Not yet! xD";
 }
