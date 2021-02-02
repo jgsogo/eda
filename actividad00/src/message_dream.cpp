a674cffab99542063445b126ed0ae43b82c408ec
jgsogo <jgsogo@gmail.com>
2021-02-02 20:22:35 +0100

    fixed: actividad00

--- actividad00/src/message_dream.cpp
+++ actividad00/src/message_dream.cpp
@@ -1,6 +1,21 @@
 
 #include "message_dream.h"
+#include <iostream>
+
+constexpr int factor_temporal = 3;
 
 std::string message_dream(std::string word, int level) {
-    return "Not implemented";
+    if (level == 0) { return word; }
+    else {
+        // Every level the letters are repeated N times
+        std::string ret;
+        for (auto c: word) {
+            int times = (c == ' ') ? 1 : factor_temporal;
+            for (auto i=0; i<times; i++) {
+                ret.push_back(c);
+            }
+        }
+        // And pass the message to the next level
+        return message_dream(ret, level - 1);
+    }
 }
--- actividad00/tests/test_message_dream.cpp
+++ actividad00/tests/test_message_dream.cpp
@@ -6,20 +6,20 @@
 SCENARIO( "Check single letter arguments", "[test_message_dream.cpp]" )
 {
     REQUIRE( message_dream("A", 0) == "A" );
-    REQUIRE( message_dream("A", 1) == "AA" );
-    REQUIRE( message_dream("A", 2) == "AAAA" );
-    REQUIRE( message_dream("A", 3) == "AAAAAAAA" );
+    REQUIRE( message_dream("A", 1) == "AAA" );
+    REQUIRE( message_dream("A", 2) == "AAAAAAAAA" );
+    REQUIRE( message_dream("A", 3) == "AAAAAAAAAAAAAAAAAAAAAAAAAAA" );
 }
 
 SCENARIO( "Check a word", "[test_message_dream.cpp]" )
 {
     REQUIRE( message_dream("hi!", 0) == "hi!" );
-    REQUIRE( message_dream("hi!", 1) == "hhii!!" );
-    REQUIRE( message_dream("hi!", 2) == "hhhhiiii!!!!" );
+    REQUIRE( message_dream("hi!", 1) == "hhhiii!!!" );
+    REQUIRE( message_dream("hi!", 2) == "hhhhhhhhhiiiiiiiii!!!!!!!!!" );
 }
 
 SCENARIO( "Check a phrase", "[test_message_dream.cpp]" )
 {
     REQUIRE( message_dream("Soy Ana", 0) == "Soy Ana" );
-    REQUIRE( message_dream("Soy Ana", 1) == "SSooyy AAnnaa" );
+    REQUIRE( message_dream("Soy Ana", 1) == "SSSoooyyy AAAnnnaaa" );
 }
