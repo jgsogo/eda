d177896189a2112af85b9eb2a3d66a733281ce1b
jgsogo <jgsogo@gmail.com>
2021-02-02 19:55:36 +0100

    add more tests

--- actividad00/tests/test_message_dream.cpp
+++ actividad00/tests/test_message_dream.cpp
@@ -3,7 +3,17 @@
 
 #include "message_dream.h"
 
-SCENARIO( "Verify no argument returns Hello World", "[test_Hello.cpp]" )
+SCENARIO( "Check single letter arguments", "[test_message_dream.cpp]" )
 {
-    REQUIRE( message_dream("Hello", 0) == "Hello World" );
+    REQUIRE( message_dream("A", 0) == "A" );
+    REQUIRE( message_dream("A", 1) == "AA" );
+    REQUIRE( message_dream("A", 2) == "AAAA" );
+    REQUIRE( message_dream("A", 3) == "AAAAAAAA" );
+}
+
+SCENARIO( "Check a word", "[test_message_dream.cpp]" )
+{
+    REQUIRE( message_dream("hi!", 0) == "hi!" );
+    REQUIRE( message_dream("hi!", 1) == "hhii!!" );
+    REQUIRE( message_dream("hi!", 2) == "hhhhiiii!!!!" );
 }
