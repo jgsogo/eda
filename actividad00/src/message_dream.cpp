77fd55b86b23fab7f6a0d5b403198172c6be3642
jgsogo <jgsogo@gmail.com>
2021-02-03 20:11:16 +0100

    match signature with slides

--- actividad00/src/message_dream.cpp
+++ actividad00/src/message_dream.cpp
@@ -1,8 +1,6 @@
 
 #include "message_dream.h"
 
-constexpr int factor_temporal = 3;
-
-std::string message_dream(std::string word, int level) {
+std::string message_dream(std::string word, int level, int factor_temporal) {
     return "Not implemented";
 }
