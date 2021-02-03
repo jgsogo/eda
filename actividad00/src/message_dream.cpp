514ba5ce8b925fb18a4bd65ee4bb1cf4748adf5b
guillermoar26 <77279628+guillermoar26@users.noreply.github.com>
2021-02-03 16:56:36 -0100

    Update message_dream.cpp

--- actividad00/src/message_dream.cpp
+++ actividad00/src/message_dream.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
-#include "message_dream.h"
+#include <string>
+#include <vector>
+using namespace std;
 
-constexpr int factor_temporal = 3;
+string message_dream(string word, int level, int factor_temporal)
+{
+    if(level == 0)
+    {
+        return word;
+    }else{
+        string ret;
+        for(auto elem: word)
+        {
+            if(elem == ' ')
+            {
+                ret.push_back(elem);
+            }else{
+                for(auto i = 0; i<factor_temporal; i++)
+                {
+                    ret.push_back(elem);
+                }
+            }
+        }
+        return message_dream(ret, level -1, factor_temporal);
+    }
+}
 
-std::string message_dream(std::string word, int level) {
-    return "Not implemented";
+int main()
+{
+    cout<<message_dream("Hola!", 2, 2)<<"\n"; // HHHHoooollllaaaa!!!!
+    cout<<message_dream("Soy Ana", 1, 3)<<"\n"; // SSSoooyyy AAAnnnaaa
 }
