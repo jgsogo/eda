ea3172369b0a43faabf25d268077fe2b1d6e4748
JMvelasco1 <78559120+JMvelasco1@users.noreply.github.com>
2021-02-04 19:53:11 +0100

    Update message_dream.cpp
    Ahora sí

--- actividad00/src/message_dream.cpp
+++ actividad00/src/message_dream.cpp
@@ -1,6 +1,26 @@
 
 #include "message_dream.h"
+#include "math.h"
+#include "string"
+
 
 std::string message_dream(std::string word, int level, int factor_temporal) {
-    return "Not implemented";
+    // TODO: Here it goes the algorithm
+    int veces= pow(factor_temporal,level);
+    std::string rep;
+    if(level==0){return word;}
+    if(level!=0){
+      for(auto elem:word){
+        if(elem == ' '){
+          rep.push_back(' ');
+        }else{
+        for(auto i=0; i<veces; i++){
+        rep.push_back(elem);
+        }
+      }
+      }
+      return rep;
+    }
+
+    return "Not implemented!";
 }
