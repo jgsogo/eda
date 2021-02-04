902a06818bb95c157cf070c9dfa10f1a565bb6df
JMvelasco1 <78559120+JMvelasco1@users.noreply.github.com>
2021-02-04 19:15:03 +0100

    Update message_dream.cpp
    No sé si así está bien

--- actividad00/src/message_dream.cpp
+++ actividad00/src/message_dream.cpp
@@ -2,5 +2,17 @@
 #include "message_dream.h"
 
 std::string message_dream(std::string word, int level, int factor_temporal) {
+    // TODO: Here it goes the algorithm
+  std::string rep;
+  if(level==0){return word;}
+  else{
+    for(auto elem:word){
+      for(auto i =0; i<factor_temporal; i++){
+        rep.push_back(elem);
+      }
+    }
+    return rep;
+  }
+
     return "Not implemented";
 }
