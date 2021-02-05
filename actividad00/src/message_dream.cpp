0e612e2c94895b7552abac105b4d4999d14ba53c
pruizj <pruizj@gmail.com>
2021-02-05 20:00:41 +0100

    Actividad

--- actividad00/src/message_dream.cpp
+++ actividad00/src/message_dream.cpp
@@ -1,6 +1,25 @@
-
-#include "message_dream.h"
+#include <iostream>
+#include <string>
 
 std::string message_dream(std::string word, int level, int factor_temporal) {
-    return "Not implemented";
+    if (level == 0) {
+        return word;
+    }else {
+        std::string new_word;
+        for (auto w: word) {
+            if( w == ' '){
+                new_word.push_back(w);
+            }else{
+                for (auto i=0; i<factor_temporal; i++) {
+                    new_word.push_back(w);
+                }
+            }
+        }
+        return message_dream(new_word, level - 1, factor_temporal);
+    }
+}
+
+int main() {
+    std::cout << message_dream("Hola!", 2, 2) << std::endl; // HHHHoooollllaaaa!!!!
+    std::cout << message_dream("Soy Ana", 1, 3) << std::endl; // SSSoooyyy AAAnnnaaa
 }
