1987828d27c89cfb9d76118fe62d9eb4e4ec316a
jgsogo <jgsogo@gmail.com>
2021-02-09 20:16:22 +0100

    push solution

--- actividad01/src/palindrome.cpp
+++ actividad01/src/palindrome.cpp
@@ -3,5 +3,11 @@
 
 bool is_palindrome(std::string word) {
     if (word.size() <= 1) { return true; }
+    else {
+        if (word.at(0) != word.at(word.size()-1)) { return false; }
+        else {
+            return is_palindrome(word.substr(1, word.size() - 2);)
+        }
+    }
     return false;
 }
