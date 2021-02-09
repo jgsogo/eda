37f47c1038d20527f1761541586034fe46c8c6fa
jgsogo <jgsogo@gmail.com>
2021-02-09 20:17:26 +0100

    typo

--- actividad01/src/palindrome.cpp
+++ actividad01/src/palindrome.cpp
@@ -6,7 +6,7 @@ bool is_palindrome(std::string word) {
     else {
         if (word.at(0) != word.at(word.size()-1)) { return false; }
         else {
-            return is_palindrome(word.substr(1, word.size() - 2);)
+            return is_palindrome(word.substr(1, word.size() - 2));
         }
     }
     return false;
