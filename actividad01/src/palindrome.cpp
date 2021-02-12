4c0b4a89e4bb27fa0821113b3dd8f38fdfa1bde9
pruizj <45947633+pruizj@users.noreply.github.com>
2021-02-12 13:27:00 +0100

    Update palindrome.cpp

--- actividad01/src/palindrome.cpp
+++ actividad01/src/palindrome.cpp
@@ -1,6 +1,23 @@
 
 #include "palindrome.h"
+#include <iostream>
+#include <vector>
 
+std::string remove_blanks(std::string word) {
+    std::string new_word;
+    for(auto v: word){
+        if(v != ' '){
+            new_word.push_back(v);
+        }
+    }
+    return new_word;
+}
+
+// Recursion: 'word' will be a palindrome if 'word[1, len-1]' is a palindrome
 bool is_palindrome(std::string word) {
+    if(word.length() <= 1){return true;}
+    if(word.at(0) == word.at(word.length()-1)){
+        return is_palindrome(word.substr(1,word.length()-2));
+    }
     return false;
 }
