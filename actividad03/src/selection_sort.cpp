a8236120c3de258c1a924ac6cb05d0c08a98ed4a
jgsogo <jgsogo@gmail.com>
2021-02-15 21:08:22 +0100

    selection sort

--- actividad03/src/selection_sort.cpp
+++ actividad03/src/selection_sort.cpp
@@ -1,6 +1,17 @@
 
 #include "sort_algorithms.h"
 
-void selection_sort(std::vector<float>& elements) {
-    std::sort(elements.begin(), elements.end());
+void selection_sort(std::vector<float> &elements)
+{
+    for (int i = 0; i < elements.size(); i++) { // 'n' iterations
+        // Search for the minimum element in the right (unordered) section
+        int indexMin = i;
+        for (int k = i + 1; k < elements.size(); k++) { // 'n-i' iterations
+            if (elements[k] < elements[indexMin]) { // keep index of min element
+                indexMin = k;
+            }
+        }
+        // Swap current element with the min
+        std::swap(elements[i], elements[indexMin]);
+    }
 }
