7e8f00e7cd6b3e50f1305f089819a19dc54fd56b
pruizj <45947633+pruizj@users.noreply.github.com>
2021-02-17 12:21:16 +0100

    Update selection_sort.cpp

--- actividad03/src/selection_sort.cpp
+++ actividad03/src/selection_sort.cpp
@@ -1,6 +1,18 @@
 
 #include "sort_algorithms.h"
+#include <iostream>
+#include <vector>
 
 void selection_sort(std::vector<float>& elements) {
-
+    for (unsigned int i=0; i<elements.size(); i++) {  // 'n' iteraciones
+        //Buscar el elemento mínimo en la sección derecha (desordenada)
+        int indexMin = i;
+        for (unsigned int k=i+1; k<elements.size(); k++) {  // 'n-i' iteraciones
+            if (elements[k] < elements[indexMin]) { // te quedas con el índice del mínimo elemento
+                indexMin = k;
+            }
+         }
+        // Intercambias el elemento actual con el mínimo
+        std::swap(elements[i], elements[indexMin]);
+    }
 }
