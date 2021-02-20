58811c5235bd97b37f32e7c285fd1fbc3719c982
David G.Muñoz <72562392+davidgregoriom@users.noreply.github.com>
2021-02-20 16:17:23 +0100

    Update Bubble_Sort_DGM.cpp

--- actividad03/src/Bubble_Sort_DGM.cpp
+++ actividad03/src/Bubble_Sort_DGM.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 #include "sort_algorithms.h"
 
-void bubble_sort(std::vector<float>& elements) {
+void bubble_sort(vector<float>& elements) {
  for (int i=0; i<elements.size()-1; i++) {
       bool any_swap = false;
-      std::cout << "Iteration " << i << std::endl;
+      cout << "Iteration " << i << endl;
       for (int k=0; k<elements.size()-1-i; k++) {
-        std::cout << elements[k] << " > " << elements[k+1] << " ? " << (elements[k] > elements[k+1] ? "true": "false") << std::endl;
+      cout << elements[k] << " > " << elements[k+1] << " ? " << (elements[k] > elements[k+1] ? "true": "false") << endl;
         if (elements[k] > elements[k+1]) {
           std::swap(elements[k], elements[k+1]);
           any_swap = true;
         }
       }
-      if (!any_swap) { break;} // return early
+      if (!any_swap) { break;} 
     }
 }
