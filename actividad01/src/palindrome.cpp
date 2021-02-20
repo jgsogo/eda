b944e8e125e7dbefaf33fc48a9a1fe77db8b521f
David G.Muñoz <72562392+davidgregoriom@users.noreply.github.com>
2021-02-20 10:46:27 +0100

    Update palindrome.cpp

--- actividad01/src/palindrome.cpp
+++ actividad01/src/palindrome.cpp
@@ -1,6 +1,29 @@
-
+#include <iostream>
+#include <string>
+#include <vector>
 #include "palindrome.h"
 
-bool is_palindrome(std::string word) {
-    return false;
+string remove_blanks(string word) {
+    string word_2;
+    for(auto c:word){
+        if(c==' '){
+            ///No necesitas hacer nada
+        }else{
+            word_2.push_back(c);
+        }
+    }
+    return word_2;
+}
+bool is_palindrome(string word) {
+    if(word.size()<2){
+        return true;
+    }else{
+        if(word.at(0)==word.at(word.size()-1)){
+             return is_palindrome(word.substr(1,word.size()-2));
+        }else{
+            return false;
+
+        }
+
+    }
 }
