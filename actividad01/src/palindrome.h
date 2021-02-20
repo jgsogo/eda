89efff792c76c4b31c2e1962aefb17aa5f96a7f7
Guillermo Ariño Gállego <77279628+guillermoar26@users.noreply.github.com>
2021-02-20 10:45:34 +0100

    Update palindrome.h

--- actividad01/src/palindrome.h
+++ actividad01/src/palindrome.h
@@ -1,3 +1,28 @@
+#include <iostream>
 #include <string>
 
-bool is_palindrome(std::string word);
+using namespace std;
+
+string remove_blanks(string word){ // Función que quita los espacios de un string
+string word_2;
+for(char elem: word){
+if(elem != ' '){
+word_2.push_back(elem);
+}
+}
+return word_2;
+}
+
+bool is_palindrome(string word){
+char primera = word.front(); char ultima = word.back();
+if(word.length() <= 1) // Caso base / trivial
+{
+return true;
+}else{
+if(primera == ultima){ // Caso general
+return is_palindrome(word.substr(1, word.length()-2));
+}else{
+return false;
+}
+}
+}
