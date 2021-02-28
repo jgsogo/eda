ed353154b5acb7124abf22b7b219f7a80d6f50f7
Guillermo Ariño Gállego <77279628+guillermoar26@users.noreply.github.com>
2021-02-28 12:51:42 +0100

    Update palindrome.cpp

--- actividad01/src/palindrome.cpp
+++ actividad01/src/palindrome.cpp
@@ -14,16 +14,21 @@ string remove_blanks(string word){ // Función que quita los espacios de un stri
    return word_2;
 }
 
-bool is_palindrome(string word){
+bool palindrome(string word){
     char primera = word.front(); char ultima = word.back();
     if(word.length() <=  1) // Caso base / trivial
     {
         return true;
     }else{
         if(primera == ultima){ // Caso general
-                return is_palindrome(word.substr(1, word.length()-2));
+                return palindrome(word.substr(1, word.length()-2));
         }else{
             return false;
         }
     }
+}
+
+bool is_palindrome(string word){
+    string word_blanks_removed = remove_blanks(word);
+    return palindrome(word_blanks_removed);
 }
