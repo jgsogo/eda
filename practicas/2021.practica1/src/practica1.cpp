74313204a6ba25ef693336c409a91fda3de1d674
Javier G. Sogo <jgsogo@gmail.com>
2021-03-06 14:10:43 +0100

    scaffolding for practica1

---
+++ practicas/2021.practica1/src/practica1.cpp
@@ -0,0 +1,82 @@
+#include "practica1.h"
+
+#include <string>
+#include <vector>
+
+/****************** 
+    Recursividad 
+******************/
+
+bool es_par(int num) {
+    return false;
+}
+
+int cociente(int dividendo, int divisor) {
+    return 0;
+}
+
+int resto(int dividendo, int divisor) {
+    return 0;
+}
+
+bool es_multiplo(int num, int divisor) {
+    return false;
+}
+
+int mcd(int a, int b) {
+    return 0;
+}
+
+int sum(int N) {
+    return 0;
+}
+
+int sum_pares(int N) {
+    return 0;
+}
+
+int sum_impares(int N) {
+    return 0;
+}
+
+std::string invertir(const std::string& input) {
+    return input;
+}
+
+int invertir(int num) {
+    return 0;
+}
+
+std::string num2binary(int num) {
+    return "";
+}
+
+bool equal(const std::vector<int>& lhs, const std::vector<int>& rhs) {
+    return false;
+}
+
+bool equal(const std::array<std::array<int, 100>, 100>& lhs, 
+           const std::array<std::array<int, 100>, 100>& rhs) {
+    return false;
+}
+
+bool is_perfect(int num) {
+    return false;
+}
+
+void bubble_sort_recursive(std::vector<int>& values) {
+
+}
+
+/****************** 
+    Algorithms 
+******************/
+
+void quicksort_strings(std::vector<std::string>& values) {
+
+}
+
+struct Point { float x, y; };
+void sort_by_distance(std::vector<Point>& values, float origin_x, float origin_y) {
+
+}
---
+++ practicas/2021.practica1/tests/test_es_par.cpp
@@ -0,0 +1,10 @@
+
+#include <catch2/catch.hpp>
+
+#include "practica1.h"
+
+
+TEST_CASE( "es_par", "[test_es_par.cpp]" )
+{
+    REQUIRE( true );
+}
