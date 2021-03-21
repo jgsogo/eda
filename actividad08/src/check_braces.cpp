d42926626b43f6635b1adcd73dab6006344ba2d4
Javier G. Sogo <jgsogo@gmail.com>
2021-03-21 18:50:00 +0100

    Solución actividad 08 (#38)
    * basic scaffolding
    
    * implementation

--- actividad08/src/check_braces.cpp
+++ actividad08/src/check_braces.cpp
@@ -3,6 +3,23 @@
 #include "Stack.h"
 
 bool check_braces(const std::string& braces) {
-    // TODO: Implement your algorithm here. Good luck!
-    return false;
+    Stack stack;
+    for (auto token: braces) {
+        if (token == '(' || token == '[') {
+            stack.push(TipoDato{token});
+        }
+        else if (token == ')' || token == ']') {
+            if (stack.size()) {
+                char top_token = stack.top().get_token();
+                stack.pop();
+                if ((token == ')' && top_token != '(') || (token == ']' && top_token != '[')) {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    return stack.size() == 0;
 }
