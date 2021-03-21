ab2f36f9313592d4830dbe8559f9dffc9cbf7ea9
Javier G. Sogo <jgsogo@gmail.com>
2021-03-21 18:46:49 +0100

    Solución actividad 03 (#36)
    * bubble sort
    
    * selection sort
    
    * working
    
    * algoritmos

--- actividad03/src/bubble_sort.cpp
+++ actividad03/src/bubble_sort.cpp
@@ -1,6 +1,14 @@
 
 #include "sort_algorithms.h"
 
-void bubble_sort(std::vector<float>& elements) {
-
+void bubble_sort(std::vector<float> &elements) {
+    if (elements.size() > 1) {
+        for (int i = 0; i < elements.size() - 1; i++) { // 'n-1' iterations
+            for (int k = 0; k < elements.size() - 1; k++) { // 'n-1' iterations
+                if (elements[k] > elements[k + 1]) { // compare adyacent values
+                    std::swap(elements[k], elements[k + 1]);
+                }
+            }
+        }
+    }
 }
--- actividad03/src/merge_sort.cpp
+++ actividad03/src/merge_sort.cpp
@@ -1,7 +1,50 @@
 
 #include "sort_algorithms.h"
 
-std::vector<float> merge_sort(const std::vector<float>& elements) {
+std::vector<float> merge(const std::vector<float>& lhs, 
+                       const std::vector<float>& rhs)
+{
     std::vector<float> ret;
+    auto left_iterator = 0;
+    auto right_iterator = 0;
+ 
+    // Iterate the two vectors at the same time, adding the lowest element
+    while (left_iterator != lhs.size() && right_iterator != rhs.size()) {
+        if (lhs[left_iterator] < rhs[right_iterator]) {
+            ret.push_back(lhs[left_iterator]);
+            left_iterator++;
+        }
+        else {
+            ret.push_back(rhs[right_iterator]);
+            right_iterator++;
+        }
+    }
+ 
+    // Add remaining elements
+    for (int i=left_iterator; i<lhs.size(); i++) {
+        ret.push_back(lhs[i]);
+    }
+    for (int i=right_iterator; i<rhs.size(); i++) {
+        ret.push_back(rhs[i]);
+    }
     return ret;
+}
+
+std::vector<float> merge_sort(const std::vector<float>& elements) {
+    if (elements.size() <= 1) {
+        return elements;
+    }
+    else {
+        // Split the vector in two
+        auto middle = elements.size()/2;
+        std::vector<float> left{elements.begin(), elements.begin() + middle};
+        std::vector<float> right{elements.begin() + middle, elements.end()};
+ 
+        // Apply merge_sort to each of them
+        left = merge_sort(left);
+        right = merge_sort(right);
+ 
+        // Merge those two (already) ordered vectors
+        return merge(left, right);
+    }
 }
